8d14e402fabf3d7ce7384acd494cf291772fe783
sruthipr027 <sruthipr027@gmail.com>
2026-01-19 14:52:50 +0530

    updtaed urls

--- controllers/cameraListController.js
+++ controllers/cameraListController.js
@@ -5,25 +5,25 @@ const getCameraList = (req, res) => {
     {
       id: "cam1",
       name: "Camera 1",
-      hlsUrl: "https://100.93.124.98:8888/cam1/index.m3u8",
+hlsUrl: "https://api.ocems.ebhoom.com/hls/cam1/index.m3u8",
       status: "offline"
     },
     {
       id: "cam2",
       name: "Camera 2",
-      hlsUrl: "https://100.93.124.98:8888/cam2/index.m3u8",
+hlsUrl: "https://api.ocems.ebhoom.com/hls/cam2/index.m3u8",
       status: "offline"
     },
     {
       id: "cam3",
       name: "Camera 3",
-      hlsUrl: "https://100.93.124.98:8888/cam3/index.m3u8",
+hlsUrl: "https://api.ocems.ebhoom.com/hls/cam3/index.m3u8",
       status: "offline"
     },
     {
       id: "cam4",
       name: "Camera 4",
-      hlsUrl: "https://100.93.124.98:8888/cam4/index.m3u8",
+hlsUrl: "https://api.ocems.ebhoom.com/hls/cam4/index.m3u8",
       status: "offline"
     }
   ];
